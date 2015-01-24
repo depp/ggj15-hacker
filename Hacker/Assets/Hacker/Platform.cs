580f98797a4422dca6b16a6a54e647a4e9b8fdfd
Dietrich Epp <depp@zdome.net>
2015-01-24 13:18:42 -0800

    Add platform class for creating platforms

---
+++ Hacker/Assets/Hacker/Platform.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour {
+	void Awake () {
+		SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+		Vector2 ssize = sprite.bounds.size;
+		Vector2 sscale = transform.localScale;
+		ssize.x /= sscale.x;
+		ssize.y /= sscale.y;
+		GameObject prefab = new GameObject();
+		SpriteRenderer csprite = prefab.AddComponent<SpriteRenderer>();
+		csprite.sprite = sprite.sprite;
+
+		Debug.Log (sscale);
+
+		GameObject child;
+		int w = (int)Mathf.Round (sscale.x), h = (int)Mathf.Round (sscale.y);
+		for (int y = 0; y < h; y++) {
+			for (int x = 0; x < w; x++) {
+				child = (GameObject) Instantiate (prefab);
+				child.transform.position = transform.position +
+					new Vector3(x * ssize.x,
+					            y * ssize.y,
+					            0);
+			}
+		}
+
+		sprite.enabled = false;
+		Destroy (prefab);
+	}
+}
