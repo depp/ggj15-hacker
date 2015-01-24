037f3e955d858edc94aeb135f7082426b3f6cba1
Dietrich Epp <depp@zdome.net>
2015-01-24 15:30:41 -0800

    Add key that you can pick up

---
+++ Hacker/Assets/Hacker/Key.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Player p = other.GetComponent<Player>();
+		if (p == null || p.hasKey)
+			return;
+		Destroy (GetComponent<Collider2D>());
+		transform.parent = other.gameObject.transform;
+		transform.localPosition = new Vector3(-0.5f, 0.5f, 0.0f);
+		p.hasKey = true;
+	}
+}
